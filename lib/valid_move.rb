2554b7887f589327d7e19b1a032ac30e03fd0008
Erin Walker <erinb0317@hotmail.com>
2018-01-06 22:43:01 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,15 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(object, index)
+  !(object[index] == "  " || object[index] == "")
+
+  if object[index] == "X" || object[index] == "O"
+    return true
+  elsif object[index] == "  "
+    return false 
+  else object[index] == nil
+    return false
+
+  end 
+end
